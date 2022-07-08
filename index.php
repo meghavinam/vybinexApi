650251ae1a7a916e556f262a330b1c30d8aa772d
Megha <meghavinam@gmail.com>
2022-07-08 11:01:37 +0530

    com

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  //$file = 'API/clients.json';
+//$json = file_get_contents($file); 
+ 
+//$json_data = json_decode($json,true);
+ 
+//echo json_encode($json_data);
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>This is the title of the webpage!</title>
+  </head>
+  <body>
+    <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, just like this <strong>p</strong> tag and its contents.</p>
+  </body>
+</html>
